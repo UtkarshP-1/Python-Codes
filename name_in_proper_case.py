4d1f9dd04275bd67e1af7f0ff40a21f8010effbb
Utkarsh Palwekar <102312432+UtkarshP-1@users.noreply.github.com>
2026-04-19 14:15:51 +0530

    Added name_in_proper_case.py
    Added code for - 
    Accept the full name from user(Name Middlename Surname) in lowercase and Print it in title case. NOTE:(Using in-built function)

---
+++ name_in_proper_case.py
@@ -0,0 +1,3 @@
+# Accept the full name from user(Name Middlename Surname) in lowercase and Print it in title case. NOTE:(Using in-built function)
+
+print(input('Enter Fullname : ').lower().title())
