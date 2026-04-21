f078551bc7dd9fe6db15f07672cab59ec22e6232
Utkarsh Palwekar <102312432+UtkarshP-1@users.noreply.github.com>
2026-04-21 15:45:18 +0530

    Create histogram.py
    Added code for - 
    Define a procedure histogram() that takes a list of integers and prints a histogram to the screen.

---
+++ histogram.py
@@ -0,0 +1,11 @@
+# Define a procedure histogram() that takes a list of integers and prints a histogram to the screen.
+# For example, histogram([4, 9, 7]) should print the following:
+# ****
+# *********
+# *******
+
+def histogram(hist: list) -> str:
+    for i in hist:
+        print('*'*i)
+        
+histogram([4,9,7])
